32fcbf5a918a3dce8e28827aa32703ea73c3740e
Xang Thao <xangthao24@yahoo.com>
2018-06-01 14:45:36 -0700

    Complete crud for project db

--- server.js
+++ server.js
@@ -3,10 +3,87 @@ const app = express();
 const cors = require("cors");
 
 const port = 5555;
+const projectDb = require("./data/helpers/projectModel");
+const actionDb = require("./data/helpers/actionModel");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello There");
 })
+
+app.get("/api/projects", (req, res) => {
+  projectDb
+    .get()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+app.get("/api/projects/:id", (req, res) => {
+  const { id } = req.params;
+  projectDb
+    .get(id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+app.post("/api/projects", (req, res) => {
+  const { name, description, completed } = req.body;
+    projectDb
+      .insert({name, description, completed})
+      .then(response => {
+        res.json(response);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+})
+
+app.delete("/api/projects/:id", (req, res) => {
+  const { id } = req.params;
+  projectDb
+    .remove(id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+app.put("/api/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, description, completed } = req.body;
+  projectDb
+    .update(id, {name, description, completed})
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+app.get("/api/projects/:project_id/actions", (req, res) => {
+  const { project_id } = req.params;
+  projectDb
+    .getProjectActions(project_id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
