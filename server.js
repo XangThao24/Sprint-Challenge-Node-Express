3f530db3a5574958a69e4475930daea45fbc3892
Xang Thao <xangthao24@yahoo.com>
2018-06-01 15:21:11 -0700

    Complete Sprint but not stretch

--- server.js
+++ server.js
@@ -84,6 +84,68 @@ app.get("/api/projects/:project_id/actions", (req, res) => {
     })
 })
 
+app.get("/api/actions", (req, res) => {
+  actionDb
+    .get()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+app.get("/api/actions/:id", (req, res) => {
+  const { id } = req.params;
+  actionDb
+    .get(id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+
+app.post("/api/actions/:project_id", (req, res) => {
+  const { project_id } = req.params;
+  const { notes, description, completed} = req.body;
+  actionDb
+    .insert({ project_id, notes, description, completed})
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+app.delete("/api/actions/:id", (req, res) => {
+  const { id } = req.params;
+  actionDb
+    .remove(id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+app.put("/api/actions/:id", (req, res) => {
+  const { id } = req.params;
+  const { notes, description, completed } = req.body;
+  actionDb
+    .update(id, {notes, description, completed})
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
